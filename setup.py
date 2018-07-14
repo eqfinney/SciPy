2c48809e1ba07994d2200307a2cb959f531463a0
Emily Quinn Finney <eqfinney@ucdavis.edu>
2018-07-14 08:37:47 -0700

    added as a package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='teentrack',
+	  version='0.2.2',
+	  description='Curriculum for the SciPy Teen Track',
+	  url='http://github.com/eqfinney/SciPy',
+	  author='Emily Quinn Finney',
+	  author_email='eqfinney@ucdavis.edu',
+	  license='MIT',
+	  packages=['teentrack'],
+	  zip_safe=False)
